a8160a213b0406d3d9727a32cc25c23fd01fc002
Rubén Chiquin <randreu28@gmail.com>
2023-04-14 00:42:21 +0200

    Creates a POST route handler

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,6 +1,6 @@
 import { createRouteHandlerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
 import { headers, cookies } from "next/headers";
+import { z } from "zod";
 
 import type { Database } from "@/lib/db.types";
 
@@ -14,5 +14,38 @@ export async function GET() {
 
   const { data } = await supabase.from("todos").select("*");
 
-  return NextResponse.json(data);
+  return new Response(JSON.stringify(data));
+}
+
+export async function POST(req: Request) {
+  const supabase = createRouteHandlerSupabaseClient<Database>({
+    headers,
+    cookies,
+  });
+
+  const todoSchema = z.object({
+    isCompleted: z.boolean().optional(),
+    title: z.string(),
+  });
+
+  const data = await req.json();
+
+  try {
+    todoSchema.parse(data);
+    const newTodo = data as z.infer<typeof todoSchema>;
+
+    const { error } = await supabase.from("todos").insert({ ...newTodo });
+
+    if (error) {
+      return new Response(JSON.stringify({ error: "Database error" }), {
+        status: 400,
+      });
+    }
+  } catch {
+    return new Response(JSON.stringify({ error: "Invalid schema" }), {
+      status: 400,
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "OK" }));
 }
