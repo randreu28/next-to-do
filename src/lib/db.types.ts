50fc7598bbddfae300100db7a78331cebad53b9f
Rubén Chiquin <randreu28@gmail.com>
2023-04-13 14:26:44 +0200

    Adds the supabase client
    Also creates a script to generate it's types with the supabaseCLI

---
+++ src/lib/db.types.ts
@@ -0,0 +1,46 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      ToDos: {
+        Row: {
+          created_at: string | null
+          id: number
+          isCompleted: boolean | null
+          title: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          isCompleted?: boolean | null
+          title?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          isCompleted?: boolean | null
+          title?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
